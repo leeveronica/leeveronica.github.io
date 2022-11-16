73ffd23433c10bff9990c11416b65ecb5801156d
leeveronica <veronica.sm.lee@gmail.com>
2022-11-16 16:27:39 -0500

    built

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/veronica.lee/Portfolio/vl-portfolio-minimal/.cache/dev-404-page.js")),
+  "component---node-modules-gatsby-theme-portfolio-minimal-src-pages-404-tsx": preferDefault(require("/Users/veronica.lee/Portfolio/vl-portfolio-minimal/node_modules/gatsby-theme-portfolio-minimal/src/pages/404.tsx"))
+}
+
